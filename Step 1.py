637b03105fb676da3b2f18a31ca6c59b3202ddc9
aug183 <vincent.nicol43@gmail.com>
2024-01-23 21:01:53 +0800

    Added a next page find

--- Step 1.py
+++ Step 1.py
@@ -1,15 +1,16 @@
-from pyexpat import features
 import requests
 from bs4 import BeautifulSoup
 
 chapternum = 1
+url = "https://bonnovel.com/manga/i-really-am-not-the-lord-of-demon-novel/chapter-642_end/"
 while (chapternum < 401):
-    #url = "https://bonnovel.com/manga/i-really-am-not-the-lord-of-demon-novel/chapter-642_end/"
-    url = "https://noveltop1.net/novel-book/the-book-eating-magician/chapter-" + str(chapternum) + "/"
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
     novel_text = soup.find("div", {"class": "reading-content"}).text
 
     chapter = open(f'Chapter{chapternum}.txt', 'a', encoding="utf-8")
     chapter.write(novel_text)
+    chapter.close()
+    a_tag = soup.find("a", {"class": "next_page"})
+    url = a_tag['href']
     chapternum += 1
