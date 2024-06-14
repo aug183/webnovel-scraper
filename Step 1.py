50bfe0044295423e74ec3442a55cc1c8b9dd33eb
aug183 <vincent.nicol43@gmail.com>
2024-06-15 02:28:49 +0800

    Added the scraping of the chapter title

--- Step 1.py
+++ Step 1.py
@@ -7,7 +7,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
 }
 url = "https://novelbin.englishnovel.net/novel-book/lord-of-mysteries-2-circle-of-inevitability/chapter-1-1-foreigners"
-while (chapternum < 782):
+for chapternum in range(1 < 782):
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.content, 'html.parser')
 
@@ -17,18 +17,23 @@ while (chapternum < 782):
     else:
         print(f"Failed to access {url}. Status code: {r.status_code}")
     
-    
+    novel_title = soup.find("a", {"class": "chr-title"})
+    if novel_title:
+        title = novel_title['title']
+
     novel_text = soup.find("div", {"class": "chr-c"})
     if novel_text:
         paragraphs = novel_text.find_all('p')
     #print(novel_text)
 
     chapter = open(f'Chapter{chapternum}.txt', 'a', encoding="utf-8")
+    chapter.write(title + '\n')
     for p in paragraphs:
         paragraphs_text = p.get_text()
         chapter.write(paragraphs_text + '\n')
     chapter.close()
+
     a_tag = soup.find("a", {"id": "next_chap"})
     url = a_tag['href']
-    chapternum += 1
-    time.sleep(2)
+    
+    time.sleep(1)
